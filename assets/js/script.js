57943eab8c496d8b8a9053698d9a85c66fa1e526
Anthony Gonzalez <shibby75@live.com>
2021-08-28 13:39:52 -0700

    added questions

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+var questions = [
+  {
+    title: "The logical operator that represents 'or' is ____.",
+    choices: ["||", "OR", "&&", "==="],
+    answer: "||",
+  },
+  {
+    title: "The condition in an if/else statement is enclosed within ______.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses",
+  },
+  {
+    title:
+      "A tool used during development and debugging for printing content to the console is:",
+    choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+    answer: "console log",
+  },
+  {
+    title: "Within a loop, what is the 'break' keyword being used for?",
+    choices: [
+      "break your competitors code",
+      "exit the loop immediately",
+      "repeat the loop",
+      "indicate a stopping condition",
+    ],
+    answer: "exit the loop immediately",
+  },
+  {
+    title: "What is a DOM?",
+    choices: [
+      "Data of Mine",
+      "Document of Master",
+      "Data Object Modal",
+      "Document Object Model",
+    ],
+    answer: "Document Object Model",
+  },
+];
