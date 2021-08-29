19f326ab164667a7b6738082c7c6e94aa424282e
Anthony Gonzalez <shibby75@live.com>
2021-08-29 12:08:21 -0700

    finished js for scores page

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -6,3 +6,18 @@ clear.addEventListener("click", function () {
   localStorage.clear();
   location.reload();
 });
+
+var totalScores = localStorage.getItem("totalScores");
+totalScores = JSON.parse(totalScores);
+
+if (totalScores !== null) {
+  for (var i = 0; i < totalScores.length; i++) {
+    var createLi = document.createElement("li");
+    createLi.textContent = totalScores[i].initials + " " + totalScores[i].score;
+    highScore.appendChild(createLi);
+  }
+}
+
+back.addEventListener("click", function () {
+  window.history.back();
+});
